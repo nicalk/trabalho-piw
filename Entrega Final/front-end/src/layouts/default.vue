34f05649be24af8d03b963818cb9b4eb5c8407d5
nicalk <lazarofreitas@alu.ufc.br>
2024-09-21 23:29:22 -0300

    menu

--- Entrega Final/front-end/src/layouts/default.vue
+++ Entrega Final/front-end/src/layouts/default.vue
@@ -1,6 +1,70 @@
 <template>
   <v-app>
     <v-main>
+      <v-card class="pa-4" flat>
+        <v-row>
+          <v-col cols="1">
+            <v-img
+              height="50"
+              src="../assets/logoMN.png"
+            />
+          </v-col>
+          <v-col cols="9" class="d-flex align-baseline">
+            <v-text-field
+              append-inner-icon="mdi-magnify"
+              density="compact"
+              variant="outlined"
+              label="Digite aqui o que voce quer pesquisar"
+              @click:append-inner=""
+            />
+          </v-col>
+          <v-col>
+            <v-card class="d-flex align-center  justify-center" flat>
+              <v-icon size="x-large">
+                mdi-account-outline
+              </v-icon>
+              <p class="pl-3">Entrar</p>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-icon size="x-large">
+              mdi-heart-outline
+            </v-icon>
+            <v-icon size="x-large" class="pl-6">
+              mdi-cart-outline
+            </v-icon>
+          </v-col>
+        </v-row>
+        <v-row class="d-flex justify-center justify-space-between pa-6">
+          <v-card flat>
+            <p>Lançamentos</p>
+          </v-card>
+          <v-card flat>
+            <p>|</p>
+          </v-card>
+          <v-card flat>
+            <p>Suplementos</p>
+          </v-card>
+          <v-card flat>
+            <p>|</p>
+          </v-card>
+          <v-card flat>
+            <p>Proteínas</p>
+          </v-card>
+          <v-card flat>
+            <p>|</p>
+          </v-card>
+          <v-card flat>
+            <p>Vitaminas e Minerais</p>
+          </v-card>
+          <v-card flat>
+            <p>|</p>
+          </v-card>
+          <v-card flat>
+            <p>Kits Promocionais</p>
+          </v-card>
+        </v-row>
+      </v-card>
       <router-view />
     </v-main>
 
@@ -8,5 +72,5 @@
 </template>
 
 <script lang="ts" setup>
-  //
+//
 </script>
